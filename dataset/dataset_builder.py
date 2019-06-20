2a348ea05b596502c1d0efd98ff409cd03df3638
LosSherl <609397880@qq.com>
2019-06-20 17:22:59 +0800

    debug

--- dataset/dataset_builder.py
+++ dataset/dataset_builder.py
@@ -45,8 +45,8 @@ class MMDataset(Dataset):
 
     def read_npy(self, index):
         row = self.images_df.iloc[index]
-        filename = os.path.basename(str(row.Id.absolute()))
-        p = os.path.join(self.vis_path.absolute(), filename + ".npy")
+        filename = os.path.basename(str(row.Id.absolute())) + ".npy"
+        p = os.path.join(self.vis_path.absolute(), filename")
         visit = np.load(p)
         return visit
 
